9cae078cae8c68ed2669bcc2ab8c725ea60c040b
Ross <528266131@qq.com>
2022-07-04 22:09:00 +0800

    feat:build dashboard prototype

--- get_sys_state_V1.0/Client/inc/Bald_Client.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BALD_CLIENT_H__
-#define __BALD_CLIENT_H__
-
-
-
-
-
-#endif
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import SideBar from "../components/SideBar";
+import Header from "../components/Header";
+import Container from "../components/Container";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <div className="flex w-screen h-screen">
+        <SideBar />
+        <div className="w-screen">
+          <Header />
+          <Container />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
