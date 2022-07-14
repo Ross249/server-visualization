99d07cf026b3a071c4da50daa1c6ab7a5ca048fc
Ross <528266131@qq.com>
2022-07-14 20:46:46 +0800

    🧪 test(test): get cpus data
    add config command line and instance to get cpus data

--- execute.config.js
+++ execute.config.js
@@ -1,31 +1,16 @@
-// import { exec } from "child_process";
+const cpuAll = {
+  cpu: [
+    {
+      e: ["wa", "id"],
+    },
+    {
+      jiffies: [
+        {
+          attr: "all",
+        },
+      ],
+    },
+  ],
+};
 
-// const config = {
-//   cpu: [
-//     {
-//       e: ["wa", "id"],
-//     },
-//     {
-//       jiffies: [
-//         {
-//           attr: "all",
-//         },
-//       ],
-//     },
-//   ],
-// };
-
-// const s = JSON.stringify(config);
-
-// const c = s.replaceAll('"', '\\"');
-
-// exec(`cd get_sys_state_V1.0 && ./client 1 1 ${c}`, (err, stdout, stderr) => {
-//   if (err) {
-//     // node couldn't execute the command
-//     console.log(err);
-//   }
-
-//   // the *entire* stdout and stderr (buffered)
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
+module.exports = cpuAll;
---
+++ test.js
@@ -0,0 +1,61 @@
+const exec = require("child_process").exec;
+const cpuAll = require("./execute.config");
+
+const s = JSON.stringify(cpuAll).replaceAll('"', '\\"');
+
+let pre;
+function run_shell_command(payloads, cb) {
+  exec(
+    `cd get_sys_state_V1.0 && ./client 1 1 "${payloads}"`,
+    function (err, stdout, stderr) {
+      if (err) {
+        cb(stderr);
+      } else {
+        cb(stdout);
+      }
+    }
+  );
+}
+
+let time = 1;
+let preIdle = 0,
+  preIowait = 0,
+  preNoIdle = 0,
+  preTotal = 0,
+  pIDLE = 0,
+  percentage = 0;
+run_shell_command(s, function (result) {
+  if (pre === undefined) {
+    pre = JSON.parse(result);
+    preIdle = pre.cpu.jiffies[0].idle;
+    preIowait = pre.cpu.jiffies[0].iowait;
+    pIDLE = preIdle + preIowait;
+    preNoIdle =
+      pre.cpu.jiffies[0].user +
+      pre.cpu.jiffies[0].nice +
+      pre.cpu.jiffies[0].sys +
+      pre.cpu.jiffies[0].irq +
+      pre.cpu.jiffies[0].softirq;
+    preTotal = pIDLE + preNoIdle;
+  } else {
+    pre = JSON.parse(result);
+    let idle = pre.cpu.jiffies[0].idle;
+    let Iowait = pre.cpu.jiffies[0].iowait;
+    let IDLE = idle + Iowait;
+    let NoIdle =
+      pre.cpu.jiffies[0].user +
+      pre.cpu.jiffies[0].nice +
+      pre.cpu.jiffies[0].sys +
+      pre.cpu.jiffies[0].irq +
+      pre.cpu.jiffies[0].softirq;
+    let total = IDLE + NoIdle;
+    percentage = (total - preTotal - (IDLE - pIDLE)) / (total - preTotal);
+    console.log((percentage * 100).toFixed(2) + "%");
+    preIdle = idle;
+    preIowait = Iowait;
+    pIDLE = IDLE;
+    preNoIdle = NoIdle;
+    preTotal = total;
+    return percentage;
+  }
+});
